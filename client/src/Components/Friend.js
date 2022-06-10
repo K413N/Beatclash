e94fb99c42045f4b751b5ac41f2517984e8b7f63
Kalen James Coulson <Kalen.Coolson@gmail.com>
2022-06-10 02:24:20 -0400

    friend component

---
+++ client/src/Components/Friend.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { Context } from "./Components/Context";
+import { useContext } from "react";
+
+const Friend = () => {
+    return(
+        <Wrapper>
+        <div>Friend</div>
+        </Wrapper>
+    )
+}
+
+export default Friend;
+
+const Wrapper = styled.div`
+margin-top: 126px;
+background-color: gray;
+position: relative;
+width: 100%;
+height: 100%;
+`
